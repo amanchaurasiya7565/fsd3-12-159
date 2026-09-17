5dfc67fe87b86470dc7f5ee979f6746d5f391bd5
amanchaurasiya7565 <amanchaurasiya7565@gmail.com>
2026-09-17 12:16:04 +0530

    user create

--- lab4/prg7.js
+++ lab4/prg7.js
@@ -1,17 +1,32 @@
 import http from "http";
-import { getUsers } from "./users.js";
+import { addUser, getUsers } from "./users.js";
 const server = http.createServer((req, res) => {
   if (req.url == "/api/users" && req.method === "GET") {
     res.end(JSON.stringify(getUsers()));
-  } else if (req.url === "/api/users" && req.method === "POST") {
-    res.end(JSON.stringify({ msg: "add user" }));
-  } else if (req.url === "/api/users/1" && req.method === "GET") {
+  }
+  else if (req.url === "/api/users" && req.method === "POST") {
+    let body='';
+    req.on('data', (chunk) => {
+      body += chunk;
+    })
+    
+    req.on('end', () => {
+      const user = JSON.parse(body);
+      const userCreated = addUser(user);
+      res.end(JSON.stringify({ msg: "user added",userCreated }));
+    });
+    
+  }
+  else if (req.url === "/api/users/1" && req.method === "GET") {
     res.end(JSON.stringify({ msg: " single user with id 1" }));
-  } else if (req.url === "/api/users/1" && req.method === "PUT") {
+  }
+  else if (req.url === "/api/users/1" && req.method === "PUT") {
     res.end(JSON.stringify({ msg: "update user 1" }));
-  } else if (req.url === "/api/users/1" && req.method === "DELETE") {
+  }
+  else if (req.url === "/api/users/1" && req.method === "DELETE") {
     res.end(JSON.stringify({ msg: "remove 1" }));
-  } else {
+  }
+  else {
     res.statusCode = 404;
     res.end();
   }
--- lab4/users.js
+++ lab4/users.js
@@ -1,9 +1,22 @@
 // We use in memory data base 
 let users =[
-    {id:1,name:"Amit Sharma",mob:"98345xxxxx",email:"amit.example@exam.com"},
-    {id:2,name:"Monika Verma",mob:"92345xxxxx",email:"moni.example@exam.com"},
+    {
+        id: 1, name: "Amit Sharma"
+        , mob: "98345xxxxx",
+        email: "amit.example@exam.com"
+    },
+    {
+        id: 2, name: "Monika Verma",
+        mob: "92345xxxxx",
+        email: "moni.example@exam.com"
+    },
 ]
 
 let nextId=3;
 
 export const getUsers = () => users;
+export const addUser = (user) => {
+    user.id = nextId++;
+    users.push(user);
+    return user;
+}
